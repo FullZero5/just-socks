e114590680733596858d83f9775c42db357de82a
@FullZero <fullzero5@gmail.com>
2025-01-10 20:30:59 +0300

    chenge text

--- src/config.ts
+++ src/config.ts
@@ -2,11 +2,11 @@
  * Configs for codrops
  */
 
-export const tutorialText = "Read the tutorial";
-export const tutorialHref = "https://tympanus.net/codrops/?p=73418";
+export const tutorialText = "About";
+export const tutorialHref = "#";
 
-export const previousText = "All demos";
-export const previousHref = "http://tympanus.net/codrops/demos/";
+export const previousText = "Policy";
+export const previousHref = "#";
 
-export const githubText   = "GitHub"
-export const githubHref   = "https://github.com/codrops/astro-shop-view-transitions"
+export const githubText   = "Telephone"
+export const githubHref   = "tel:+7 (999) 999-99-99"
